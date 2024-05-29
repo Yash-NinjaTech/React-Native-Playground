4bc2372c460aa35744069224eaaaa9bc3fe8dfb2
Yash-NT <yash@ninjatechnolabs.com>
2024-05-29 20:44:09 +0530

    setup activity agent

--- App.tsx
+++ App.tsx
@@ -1,8 +1,23 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import StackNavigation from './src/navigation/Stack';
+import React, {useEffect} from 'react';
+import StackNavigation from '@/src/navigation/Stack';
+import {makeActivitySearchAgent} from './src/utils/agents/makeActivitiesAgent';
 
 function App(): JSX.Element {
+  const fetchData = async () => {
+    try {
+      const agent = await makeActivitySearchAgent();
+      console.log('agent', agent);
+    } catch (error) {
+      console.log('Error', error)
+    }
+  };
+
+  useEffect(() => {
+    console.log('fetchData Call')
+    fetchData();
+  }, []);
+
   return (
     <NavigationContainer>
       <StackNavigation />
--- simplenight-sdk/api/apiCore.ts
+++ simplenight-sdk/api/apiCore.ts
@@ -1,4 +1,4 @@
-import { ApiCore as ApiCoreBase, OpenAPIConfig } from "./core";
+import {ApiCore as ApiCoreBase, OpenAPIConfig} from './core';
 
 export class ApiCore extends ApiCoreBase {
   constructor(config?: Partial<OpenAPIConfig>) {
@@ -7,7 +7,7 @@ export class ApiCore extends ApiCoreBase {
     }
     if (!config.BASE) {
       //   const apiUrl = process.env.NEXT_PUBLIC_API || ""
-      const apiUrl = "https://api.dev.v4.simplenight.com";
+      const apiUrl = 'https://api.dev.v4.simplenight.com';
       config.BASE = apiUrl;
     }
 
